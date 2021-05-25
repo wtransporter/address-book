0a94520ebbb7041746432ea67a72492a9635a788
vlada <vladapantic@hotmail.com>
2021-05-25 14:49:27 +0200

    Add confirmation dialog on item delete

--- src/components/contact-resources/SingleContact.vue
+++ src/components/contact-resources/SingleContact.vue
@@ -1,4 +1,14 @@
 <template>
+  <base-alert v-if="confirmDelete" @close="cancelAction" 
+    title="Confirm ?">
+    <template #default>
+      Are you sure you want to delete this item ?
+    </template>
+    <template #actions>
+      <base-button @click="confirmAction" type="danger">Delete</base-button>
+      <base-button @click="cancelAction">Cancel</base-button>
+    </template>
+  </base-alert>
   <span class="flex justify-between border-b mb-1">
     <div>
       <h3 class="text-xl">{{ name }}</h3>
@@ -8,7 +18,7 @@
       </p>
     </div>
     <div>
-      <base-button @click="deleteItem(id)" type="danger">
+      <base-button @click="canDeleteItem" type="danger">
         Delete
       </base-button>
     </div>
@@ -20,6 +30,27 @@ export default {
   name: 'SingleContact',
   props: ['id', 'name', 'phone', 'address'],
 
-  inject: ['deleteItem']
+  inject: ['deleteItem'],
+
+  data() {
+    return {
+      confirmDelete: false
+    }
+  },
+
+  methods: {
+    canDeleteItem() {
+      this.confirmDelete = true;
+    },
+
+    confirmAction() {
+      this.confirmDelete = false;
+      this.deleteItem(this.id);
+    },
+
+    cancelAction() {
+      this.confirmDelete = false
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import BaseCard from './components/ui/BaseCard'
-import BaseButton from './components/ui/BaseButton'
+import { createApp } from "vue";
+import App from "./App.vue";
+import BaseCard from "./components/ui/BaseCard";
+import BaseButton from "./components/ui/BaseButton";
+import BaseAlert from "./components/ui/BaseAlert";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.component('base-card', BaseCard)
-app.component('base-button', BaseButton)
+app.component("base-card", BaseCard);
+app.component("base-button", BaseButton);
+app.component("base-alert", BaseAlert);
 
-app.mount('#app')
+app.mount("#app");
